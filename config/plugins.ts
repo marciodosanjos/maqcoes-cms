3b07a6ee8bb2c6941fa6e0e3e8f96c97134277cb
Marcio <marciodosanjos@ymail.com>
2024-11-26 13:07:09 -0300

    handle jwtSecret

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+module.exports = {
+  "users-permissions": {
+    config: {
+      jwtSecret: process.env.JWT_SECRET, // Lê a chave do JWT a partir da variável de ambiente
+    },
+  },
+};
